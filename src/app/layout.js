1c36481fa1c5e853334fb5796b43ebf492a55e02
mauroNbome <mauro3196@gmail.com>
2024-08-17 21:48:02 -0300

    add footer component

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,8 @@
-import { Inter } from "next/font/google";
-import "./globals.css";
+import { Inter } from 'next/font/google';
+import './globals.css';
+import Footer from '@/app/components/Footer';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
   title: "Google Clone",
@@ -10,8 +11,11 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+    <html lang='en'>
+    <body className={inter.className}>
+      {children}
+      <Footer />
+    </body>
+  </html>
   );
 }
